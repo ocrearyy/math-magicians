fe301cbd30be83942f31c7e8037bc721942c60a4
oshanedesign <oshane.design@gmail.com>
2022-04-20 15:27:01 -0500

    implemented functionality

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import operate from '../logic/operate';
 import calculate from '../logic/calculate';
 
 export default class Calculator extends Component {
@@ -8,40 +7,57 @@ export default class Calculator extends Component {
     this.state = {};
   }
 
+  handleEvent = (e) => {
+    const button = e.target.textContent;
+    const value = calculate(this.state, button);
+    this.setState(value);
+  }
+
   render() {
+    const { total, operation, next } = this.state;
+    const getResult = () => {
+      let result = 0;
+      if (total) {
+        result = `${total} ${operation || ''} ${next || ''}`;
+      } else if (next) {
+        result = next;
+      }
+      return result;
+    };
+
     return (
       <div className="calculator">
         <div className="input-wrap">
           <div className="output">
-            <p className="input-text">0</p>
+            <p className="input-text">{getResult()}</p>
           </div>
           <div className="button-wrap">
             {/* row 1 */}
-            <button className="function" type="button">AC</button>
-            <button className="function" type="button">+/-</button>
-            <button className="function" type="button">%</button>
-            <button className="operator" type="button">÷</button>
+            <button className="function" type="button" onClick={this.handleEvent}>AC</button>
+            <button className="function" type="button" onClick={this.handleEvent}>+/-</button>
+            <button className="function" type="button" onClick={this.handleEvent}>%</button>
+            <button className="operator" type="button" onClick={this.handleEvent}>÷</button>
             {/* row 2 */}
-            <button className="number" type="button">7</button>
-            <button className="number" type="button">8</button>
-            <button className="number" type="button">9</button>
-            <button className="operator" type="button">x</button>
+            <button className="number" type="button" onClick={this.handleEvent}>7</button>
+            <button className="number" type="button" onClick={this.handleEvent}>8</button>
+            <button className="number" type="button" onClick={this.handleEvent}>9</button>
+            <button className="operator" type="button" onClick={this.handleEvent}>x</button>
             {/* row 3 */}
-            <button className="number" type="button">4</button>
-            <button className="number" type="button">5</button>
-            <button className="number" type="button">6</button>
-            <button className="operator" type="button">-</button>
+            <button className="number" type="button" onClick={this.handleEvent}>4</button>
+            <button className="number" type="button" onClick={this.handleEvent}>5</button>
+            <button className="number" type="button" onClick={this.handleEvent}>6</button>
+            <button className="operator" type="button" onClick={this.handleEvent}>-</button>
             {/* row 4 */}
-            <button className="number" type="button">1</button>
-            <button className="number" type="button">2</button>
-            <button className="number" type="button">3</button>
-            <button className="operator" type="button">+</button>
+            <button className="number" type="button" onClick={this.handleEvent}>1</button>
+            <button className="number" type="button" onClick={this.handleEvent}>2</button>
+            <button className="number" type="button" onClick={this.handleEvent}>3</button>
+            <button className="operator" type="button" onClick={this.handleEvent}>+</button>
           </div>
           {/* row 5 */}
           <div className="final-row">
-            <button className="number" type="button">0</button>
-            <button className="function" type="button">.</button>
-            <button className="operator" type="button">=</button>
+            <button className="number" type="button" onClick={this.handleEvent}>0</button>
+            <button className="function" type="button" onClick={this.handleEvent}>.</button>
+            <button className="operator" type="button" onClick={this.handleEvent}>=</button>
           </div>
         </div>
       </div>
