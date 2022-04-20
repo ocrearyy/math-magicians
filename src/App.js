679ac12e3604f6dca2f66c603740ccc34f5f972e
oshanedesign <oshane.design@gmail.com>
2022-04-20 10:54:59 -0500

    fix issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Calculator from './component/Calculator.js';
+import Calculator from './component/Calculator';
 
 class App extends React.Component {
   constructor(props) {
