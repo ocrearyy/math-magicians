817c7b252914b6446d19f39e3cd3c52195762aa2
oshanedesign <oshane.design@gmail.com>
2022-04-30 12:05:22 -0500

    update css home page

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,11 +1,25 @@
+import '../App.css';
+
 const Home = () => (
   <div className="home">
     <h2 className="heading">Welcome to our page!</h2>
-    <p>
+    <p className="lorem">
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore corporis
       deserunt repellendus vero optio vitae,
       cum nisi cupiditate vel fugiat dolorem voluptate?
       Temporibus autem quos odio quas unde nesciunt veniam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum repellat similique qui, sunt,
+      velit fugiat asperiores eveniet magnam facilis,
+      mollitia cum hic. Suscipit et ducimus provident neque repellat. Quaerat, cumque!
+    </p>
+    <p className="lorem">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore corporis
+      deserunt repellendus vero optio vitae,
+      cum nisi cupiditate vel fugiat dolorem voluptate?
+      Temporibus autem quos odio quas unde nesciunt veniam?
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum repellat similique qui, sunt,
+      velit fugiat asperiores eveniet magnam facilis,
+      mollitia cum hic. Suscipit et ducimus provident neque repellat. Quaerat, cumque!
     </p>
   </div>
 );
